f3f04181490730ec41ff7c62053c87e466e7775c
kemoeverlyne <kemoeverlyne@gmail.com>
2024-09-14 17:47:56 +0300

    bug fix: improve routes

--- api-gateway/app/routers/complete.py
+++ api-gateway/app/routers/complete.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
+import httpx
 
 router = APIRouter()
 
 @router.post("/complete")
-async def complete_text(text: str):
-    # Call the completion microservice
-    return {"completion": f"Completed version of '{text}'"}
+async def complete_text(prompt: str, max_length: int = 100):
+    async with httpx.AsyncClient() as client:
+        response = await client.post("http://localhost:8002/completion", json={"prompt": prompt, "max_length": max_length})
+        return {"complete Text": f"Complete version of '{prompt}'"}
--- api-gateway/app/routers/summarize.py
+++ api-gateway/app/routers/summarize.py
@@ -1,8 +1,11 @@
 from fastapi import APIRouter
+import httpx
 
 router = APIRouter()
 
 @router.post("/summarize")
-async def summarize(text: str):
-    # Call the summarization microservice
+async def summarize(text: str, max_length: int= 100):
+    async with httpx.AsyncClient() as client:
+        response = await client.post("http://localhost:8001/summarize", json={"text": text, "max_length": max_length})
     return {"summary": f"Summarized version of '{text}'"}
+    # Call the summarization microservice
